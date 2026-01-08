c036fd82d96baecc8d775c5bf00e12b38d30aed4
Anita Derebecka <anita.derebecka@rebike.com>
2026-01-08 22:41:39 +0100

    ops(ci/cd): PWD-1003: integrate with CI
    - created pipeline with env. selection and tests type (smoke, regression) - just for ilustration
    - added .env to gitignore
    - added @smoke tag to test

--- tests/header/login.spec.ts
+++ tests/header/login.spec.ts
@@ -1,9 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { expect, test } from './../../fixtures/pageFixtures'
-import {
-  AuthModal,
-  type Credentials as UserCredentials
-} from '../../components/authModal'
+import { AuthModal, type Credentials as UserCredentials } from '../../components/authModal'
 import { State } from '../../pages/pageObjects'
 import * as texts from '../../locales/common.json'
 import { CurrentUser } from '../../models/currentUser'
@@ -15,18 +12,12 @@ const validCredentials: UserCredentials = {
 
 const errorMessage = texts.account.invalidCredentialsError
 
-const assertUserLoggedIn = async (
-  authModalComponent: AuthModal,
-  currentUser: CurrentUser
-): Promise<void> => {
+const assertUserLoggedIn = async (authModalComponent: AuthModal, currentUser: CurrentUser): Promise<void> => {
   await authModalComponent.modalBody.waitFor({ state: State.hidden })
   await expect(await currentUser.getEmail()).toBe(validCredentials.email)
 }
 
-const assertErrorDisplayed = async (
-  authModalComponent: AuthModal,
-  expectedError: string
-): Promise<void> => {
+const assertErrorDisplayed = async (authModalComponent: AuthModal, expectedError: string): Promise<void> => {
   await expect(authModalComponent.errorText).toHaveText(expectedError)
 }
 
@@ -37,34 +28,26 @@ test.beforeEach(async ({ homePage, header }) => {
 
 test(
   'success login with valid credentials',
-  { tag: ['@auth', '@critical'] },
+  { tag: ['@auth', '@critical', '@smoke'] },
   async ({ authModal, currentUser }) => {
     await authModal.login(validCredentials)
     await assertUserLoggedIn(authModal, currentUser)
   }
 )
 
-test(
-  'failed login with invalid email address',
-  { tag: ['@auth', '@critical'] },
-  async ({ authModal }) => {
-    const credentials = { ...validCredentials }
-    credentials.email = faker.internet.email()
-    await authModal.login(credentials)
-    await assertErrorDisplayed(authModal, errorMessage)
-  }
-)
+test('failed login with invalid email address', { tag: ['@auth', '@critical'] }, async ({ authModal }) => {
+  const credentials = { ...validCredentials }
+  credentials.email = faker.internet.email()
+  await authModal.login(credentials)
+  await assertErrorDisplayed(authModal, errorMessage)
+})
 
-test(
-  'failed login with invalid password address',
-  { tag: ['@auth', '@critical'] },
-  async ({ authModal }) => {
-    const credentials = { ...validCredentials }
-    credentials.password = faker.internet.password()
-    await authModal.login(credentials)
-    await expect(authModal.errorText).toHaveText(errorMessage)
-  }
-)
+test('failed login with invalid password address', { tag: ['@auth', '@critical'] }, async ({ authModal }) => {
+  const credentials = { ...validCredentials }
+  credentials.password = faker.internet.password()
+  await authModal.login(credentials)
+  await expect(authModal.errorText).toHaveText(errorMessage)
+})
 
 test(
   'error display on login attempt without providing credentials',
@@ -76,10 +59,7 @@ test(
   }
 )
 
-test('success login after incorrect credentials update', async ({
-  authModal,
-  currentUser
-}) => {
+test('success login after incorrect credentials update', async ({ authModal, currentUser }) => {
   const credentials = { ...validCredentials }
   credentials.password = faker.internet.password()
   await authModal.login(credentials)
