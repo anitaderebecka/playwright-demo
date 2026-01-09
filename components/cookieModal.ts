e8921bdc705f1d54d6e797d154d3570e67fb2c44
Anita Derebecka <53033174+anitaderebecka@users.noreply.github.com>
2026-01-09 08:18:03 +0100

    Pwd 1003 integrate with pipeline (#4)
    * ops(ci/cd): PWD-1003: integrate with CI
    
    - created pipeline with env. selection and tests type (smoke, regression) - just for ilustration
    - added .env to gitignore
    - added @smoke tag to test

--- components/cookieModal.ts
+++ components/cookieModal.ts
@@ -3,7 +3,10 @@ import { Locator, type Page } from '@playwright/test'
 export class CookieModal {
   readonly cookiesAcceptBtn: Locator
 
-  constructor(private page: Page) {
-    this.cookiesAcceptBtn = this.page.locator('#qc-cmp2-ui #accept-btn')
+  constructor(private page: Page) {}
+
+  async acceptCookiesIfShown() {
+    const geoLocation = await (await this.page.request.get('https://get.geojs.io/v1/ip/geo.json')).json()
+    if (geoLocation.continent_code === 'EU') await this.page.locator('#qc-cmp2-ui #accept-btn').click()
   }
 }
